f2b3a3a6709267f20f6bc333083e00b1ff575edb
Sherwood Glazier <sherwood.glazier@roguewave.com>
2021-02-17 11:21:54 -0700

    removing AHS info

--- install-base.sh
+++ install-base.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-INSTALL_PATH="/data/u01/apps/Akana/2020"
+# INSTALL_PATH - Where the product will be installed
+INSTALL_PATH="/opt/akana/sandbox/2020.1"
+# SOURCE_PATH - Where the product installation files are located.
+SOURCE_PATH="/opt/akana/resources/2020.1"
 #where it can find the zip files, etc that are downloaded, should standardize this location
-SOURCE_PATH="/data/u01/apps/Akana/resources"
-#where it can find the zip files, etc that are downloaded, should standardize this location
-AUTOMATION_FILES="/data/u01/apps/Akana/automation"
+AUTOMATION_FILES="/opt/akana/sandbox/automation_2020.1"
 AKANA_ZIP=("akana-platform-linux-jre_2020.1.0.zip" "akana-api-platform_2020.1.0.zip" "albertahealthservices-loggers-policy_2.0.0_0909.zip")
 
 #unzip the akana-platform & akana-api-platform
@@ -12,8 +13,8 @@ do
 	unzip $SOURCE_PATH/$i -d $INSTALL_PATH/
 done
 
-#unzip updated jce
-unzip -j $SOURCE_PATH/jce_policy-8.zip -d $INSTALL_PATH/jre/lib/security/
+#unzip updated jce - NO LONGER NEEDED AS OF JDK-1.8.262
+#unzip -j $SOURCE_PATH/jce_policy-8.zip -d $INSTALL_PATH/jre/lib/security/
 
 #unzip custom recipes to INSTALL_PATH/recipes folder
 unzip -o $AUTOMATION_FILES/ps-recipes.zip -d $INSTALL_PATH/recipes/
